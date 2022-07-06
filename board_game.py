f9fd1c6afe993533470ee387dcef909a7fb38088
Seongmin Moon <tjdals1206@hufs.ac.kr>
2022-07-06 19:12:10 +0900

    base code

---
+++ board_game.py
@@ -0,0 +1,50 @@
+class ComputerPlayer:
+
+    balance = 5000
+    place = ""
+
+    def move(self):
+
+    def buy(self):
+
+    def pay(self):
+
+    def receive(self):
+
+    def is_bankrupt(self):
+
+
+city_name = ["Start"]
+
+class Board:
+    def __init__(self):
+        self.city = []
+        self. player = []
+        self.create()
+
+    def create(self):
+        for i in city_name:
+            self.city.append(i)
+
+
+class Player: #real player
+
+    name = ""
+    balance = 5000
+    place = ""
+
+    def getName(self):
+
+    def getPlace(self):
+
+    def getBalance(self):
+
+    def move(self):
+
+    def buy(self):
+
+    def pay(self):
+
+    def receive(self):
+
+    def is_bankrupt(self):
