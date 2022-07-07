223532ae8965b4c51351980b299db269ca6c1855
Seongmin Moon <tjdals1206@hufs.ac.kr>
2022-07-07 15:48:38 +0900

    add player buy,pay

--- board_game.py
+++ board_game.py
@@ -44,9 +44,22 @@ class Player: #real player
     def getBalance(self):
         return self.balance
 
-    def move(self):
-
-    def buy(self):
+    def buy(self,price,city_name): #이동한 도시가 비어있고, 잔고 충분하다면 도시를 구매
+        if self.balance >= price:
+            print(self.name + " 이(가) " + self.city_name + "을(를) 구매했습니다.")
+            self.balance -= price
+            print(self.name + "의 잔고가 " + self.balance + "만큼 남았습니다.")
+        else:
+            print("잔고가 부족합니다. 도시를 구매하지 못했습니다.")
+    
+    def pay(self,price): #이동한 도시의 주인이 있을 때 통행료을 지불하고 잔고가 부족하다면 표시
+        if self.owner == "player":
+            if self.balance >= price:
+                print(self.name + "이(가)" + self.city_name + "을(를) 구매했습니다.")
+                self.balance -= price
+                print(self.name + " 의 잔고가 " + self.balance + "만큼 남았습니다.")
+            else:
+                print("잔고가 부족합니다."+ self.name + "이(가) 대금을 지불하지 못했습니다.")
 
     def pay(self):
 
